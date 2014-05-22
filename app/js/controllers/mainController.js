22b52fe23216f7c4cc06a5aa0dbdd78a0e225146
thetallweeks <kevinmichaelweeks@gmail.com>
2014-05-22 00:30:27 -0700

    Set up default angular app with firebase

---
+++ app/js/controllers/mainController.js
@@ -0,0 +1,17 @@
+(function (controllerId) {
+  'use strict';
+
+  var controller = function($scope, $firebase, firebaseUrl) {
+    var peopleRef = new Firebase(firebaseUrl + '/people');
+    $scope.people = $firebase(peopleRef);
+    $scope.addPerson = function() {
+    // AngularFire $add method
+    $scope.people.$add($scope.newPerson);
+    $scope.newPerson = "";
+  }
+}
+
+controller.$inject = ['$scope', '$firebase', 'firebaseUrl'];
+angular.module('app').controller(controllerId, controller);
+
+})('app.mainController');
